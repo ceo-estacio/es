b419cc5b144634f2a7f90b2b06134fd138c54006
@Ceo-Sammarco <noah.kd@gmail.com>
2024-08-12 18:45:59 -0300

    @Ceo-Sammarco - Mon 12 Aug 2024

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,40 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+
+
+// const tintColorLight = "#2f95dc";
+// const tintColorDark = "#fff";
+const 
+   light = {
+      color: {
+         tint: "#2f95dc",
+         iconDefault: "#27f",
+         text: "#333",
+         background: "#f5f5f5",
+      }
+   }
+   ,
+   dark = {
+      color: {
+         tint: "#fff",
+         iconDefault: "#27f",
+         text: "#eee",
+         background: "#212329",
+      }
+   }
+;
 
 export default {
-  light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
-  },
+   light: {
+      text: light.color.text,
+      background: light.color.background,
+      tint: light.color.tint,
+      tabIconDefault: light.color.iconDefault,
+      tabIconSelected: light.color.tint,
+   },
+   dark: {
+      text: dark.color.text,
+      background: dark.color.background,
+      tint: dark.color.tint,
+      tabIconDefault: dark.color.iconDefault,
+      tabIconSelected: dark.color.tint,
+   },
 };
